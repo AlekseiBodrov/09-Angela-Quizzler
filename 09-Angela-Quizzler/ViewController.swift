0492310525f9817b30d554ee5e25c31719c089a0
Aleksei <aleksei@mail.ru>
2023-01-30 21:17:49 +0300

    Initial commit

---
+++ 09-Angela-Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  09-Angela-Quizzler
+//
+//  Created by Алексей on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
